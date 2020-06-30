e368809c97d1f91f40635d0718cd95373d109248
Mark Anthony Naluz <mnaluz@teko.ph>
2020-06-30 22:15:41 +0800

    pdf path

--- application/views/pages/student_requirements.php
+++ application/views/pages/student_requirements.php
@@ -44,7 +44,7 @@
                 <?php
                 if(!empty($fetch_data['resume'])) {
 				  	if($fetch_data['resume'] != 'no_pdf.png') {
-				  		echo '<a href="'.base_url().'assets/pdf/'.$fetch_data['resume'].'" target="_blank"><img src="'.base_url().'assets/pdf/pdf.png" width="150px"></a>
+				  		echo '<a href="'.base_url($fetch_data['resume']).'" target="_blank"><img src="'.base_url().'assets/pdf/pdf.png" width="150px"></a>
 				    	<legend>'.$fetch_data['resume'].'</legend>';
 				    	if($fetch_data['resume_status'] == 0) {
 				    		echo '<div class="btn-group btn-group-toggle" data-toggle="buttons">';
@@ -70,19 +70,22 @@
             	<center>
 			  	<br>
                 <?php 
-			  	if($fetch_data['clearance'] != 'no_pdf.png') {
-			  		echo '<a href="'.base_url().'assets/pdf/'.$fetch_data['clearance'].'" target="_blank"><img src="'.base_url().'assets/pdf/pdf.png" width="150px"></a>
-			    	<legend>'.$fetch_data['clearance'].'</legend>';
-			    	if($fetch_data['clearance_status'] == 0) {
-			    		echo '<div class="btn-group btn-group-toggle" data-toggle="buttons">';
-			    		echo '<a href="'.base_url('admin/confirm_clearance/'.$fetch_data['username'].'').'" class="btn btn-success"><i class="fas fa-check"></i></a>';
-			    		echo '<a href="'.base_url('admin/decline_clearance/'.$fetch_data['username'].'').'" class="btn btn-danger"><i class="fas fa-times"></i></a>';
-			    		echo '</div>';
-			    	}
-			    	else{
-			    		echo 'Status: Approved';
-			    	}
-			    	
+                if(!empty($fetch_data['clearance'])) {
+				  	if($fetch_data['clearance'] != 'no_pdf.png') {
+				  		echo '<a href="'.base_url($fetch_data['clearance']).'" target="_blank"><img src="'.base_url().'assets/pdf/pdf.png" width="150px"></a>
+				    	<legend>'.$fetch_data['clearance'].'</legend>';
+				    	if($fetch_data['clearance_status'] == 0) {
+				    		echo '<div class="btn-group btn-group-toggle" data-toggle="buttons">';
+				    		echo '<a href="'.base_url('admin/confirm_clearance/'.$fetch_data['studentID'].'').'" class="btn btn-success"><i class="fas fa-check"></i></a>';
+				    		echo '<a href="'.base_url('admin/decline_clearance/'.$fetch_data['studentID'].'').'" class="btn btn-danger"><i class="fas fa-times"></i></a>';
+				    		echo '</div>';
+				    	}
+				    	else{
+				    		echo 'Status: Approved';
+				    	}
+				    } else {
+				    	echo '<legend>No PDF Uploaded</legend>';
+				    }
 			  	}
 			  	else{
 			  		echo '<legend>No PDF Uploaded</legend>';
@@ -94,19 +97,23 @@
             <div class="tab-pane fade" id="waiver">
             	<center>
 			  	<br>
-                <?php 
-			  	if($fetch_data['waiver'] != 'no_pdf.png') {
-			  		echo '<a href="'.base_url().'assets/pdf/'.$fetch_data['waiver'].'" target="_blank"><img src="'.base_url().'assets/pdf/pdf.png" width="150px"></a>
-			    	<legend>'.$fetch_data['waiver'].'</legend>';
-			    	if($fetch_data['waiver_status'] == 0) {
-			    		echo '<div class="btn-group btn-group-toggle" data-toggle="buttons">';
-			    		echo '<a href="'.base_url('admin/confirm_waiver/'.$fetch_data['username'].'').'" class="btn btn-success"><i class="fas fa-check"></i></a>';
-			    		echo '<a href="'.base_url('admin/decline_waiver/'.$fetch_data['username'].'').'" class="btn btn-danger"><i class="fas fa-times"></i></a>';
-			    		echo '</div>';
-			    	}
-			    	else{
-			    		echo 'Status: Approved';
-			    	}
+                <?php
+                if(!empty($fetch_data['waiver'])) {
+				  	if($fetch_data['waiver'] != 'no_pdf.png') {
+				  		echo '<a href="'.base_url($fetch_data['waiver']).'" target="_blank"><img src="'.base_url().'assets/pdf/pdf.png" width="150px"></a>
+				    	<legend>'.$fetch_data['waiver'].'</legend>';
+				    	if($fetch_data['waiver_status'] == 0) {
+				    		echo '<div class="btn-group btn-group-toggle" data-toggle="buttons">';
+				    		echo '<a href="'.base_url('admin/confirm_waiver/'.$fetch_data['username'].'').'" class="btn btn-success"><i class="fas fa-check"></i></a>';
+				    		echo '<a href="'.base_url('admin/decline_waiver/'.$fetch_data['username'].'').'" class="btn btn-danger"><i class="fas fa-times"></i></a>';
+				    		echo '</div>';
+				    	}
+				    	else{
+				    		echo 'Status: Approved';
+				    	}
+				    } else {
+				    	echo '<legend>No PDF Uploaded</legend>';
+				    }
 			    	
 			  	}
 			  	else{
@@ -119,19 +126,23 @@
             <div class="tab-pane fade" id="good_moral">
             	<center>
 			  	<br>
-                <?php 
-			  	if($fetch_data['good_moral'] != 'no_pdf.png') {
-			  		echo '<a href="'.base_url().'assets/pdf/'.$fetch_data['good_moral'].'" target="_blank"><img src="'.base_url().'assets/pdf/pdf.png" width="150px"></a>
-			    	<legend>'.$fetch_data['good_moral'].'</legend>';
-			    	if($fetch_data['good_moral_status'] == 0) {
-			    		echo '<div class="btn-group btn-group-toggle" data-toggle="buttons">';
-			    		echo '<a href="'.base_url('admin/confirm_good_moral/'.$fetch_data['username'].'').'" class="btn btn-success"><i class="fas fa-check"></i></a>';
-			    		echo '<a href="'.base_url('admin/decline_good_moral/'.$fetch_data['username'].'').'" class="btn btn-danger"><i class="fas fa-times"></i></a>';
-			    		echo '</div>';
-			    	}
-			    	else{
-			    		echo 'Status: Approved';
-			    	}
+                <?php
+                if(!empty($fetch_data['good_moral'])) {
+				  	if($fetch_data['good_moral'] != 'no_pdf.png') {
+				  		echo '<a href="'.base_url($fetch_data['good_moral']).'" target="_blank"><img src="'.base_url().'assets/pdf/pdf.png" width="150px"></a>
+				    	<legend>'.$fetch_data['good_moral'].'</legend>';
+				    	if($fetch_data['good_moral_status'] == 0) {
+				    		echo '<div class="btn-group btn-group-toggle" data-toggle="buttons">';
+				    		echo '<a href="'.base_url('admin/confirm_good_moral/'.$fetch_data['username'].'').'" class="btn btn-success"><i class="fas fa-check"></i></a>';
+				    		echo '<a href="'.base_url('admin/decline_good_moral/'.$fetch_data['username'].'').'" class="btn btn-danger"><i class="fas fa-times"></i></a>';
+				    		echo '</div>';
+				    	}
+				    	else{
+				    		echo 'Status: Approved';
+				    	}
+				    } else {
+				    	echo '<legend>No PDF Uploaded</legend>';
+				    }
 			    	
 			  	}
 			  	else{
@@ -144,19 +155,23 @@
             <div class="tab-pane fade" id="registration_form">
             	<center>
 			  	<br>
-                <?php 
-			  	if($fetch_data['registration_form'] != 'no_pdf.png') {
-			  		echo '<a href="'.base_url().'assets/pdf/'.$fetch_data['registration_form'].'" target="_blank"><img src="'.base_url().'assets/pdf/pdf.png" width="150px"></a>
-			    	<legend>'.$fetch_data['registration_form'].'</legend>';
-			    	if($fetch_data['registration_status'] == 0) {
-			    		echo '<div class="btn-group btn-group-toggle" data-toggle="buttons">';
-			    		echo '<a href="'.base_url('admin/confirm_registration/'.$fetch_data['username'].'').'" class="btn btn-success"><i class="fas fa-check"></i></a>';
-			    		echo '<a href="'.base_url('admin/decline_registration/'.$fetch_data['username'].'').'" class="btn btn-danger"><i class="fas fa-times"></i></a>';
-			    		echo '</div>';
-			    	}
-			    	else{
-			    		echo 'Status: Approved';
-			    	}
+                <?php
+                if(!empty($fetch_data['registration_form'])) {
+				  	if($fetch_data['registration_form'] != 'no_pdf.png') {
+				  		echo '<a href="'.base_url($fetch_data['registration_form']).'" target="_blank"><img src="'.base_url().'assets/pdf/pdf.png" width="150px"></a>
+				    	<legend>'.$fetch_data['registration_form'].'</legend>';
+				    	if($fetch_data['registration_status'] == 0) {
+				    		echo '<div class="btn-group btn-group-toggle" data-toggle="buttons">';
+				    		echo '<a href="'.base_url('admin/confirm_registration/'.$fetch_data['username'].'').'" class="btn btn-success"><i class="fas fa-check"></i></a>';
+				    		echo '<a href="'.base_url('admin/decline_registration/'.$fetch_data['username'].'').'" class="btn btn-danger"><i class="fas fa-times"></i></a>';
+				    		echo '</div>';
+				    	}
+				    	else{
+				    		echo 'Status: Approved';
+				    	}
+				    } else {
+				    	echo '<legend>No PDF Uploaded</legend>';
+				    }
 			    	
 			  	}
 			  	else{
@@ -169,20 +184,23 @@
             <div class="tab-pane fade" id="parents_consent">
             	<center>
 			  	<br>
-                <?php 
-			  	if($fetch_data['parents_consent'] != 'no_pdf.png') {
-			  		echo '<a href="'.base_url().'assets/pdf/'.$fetch_data['parents_consent'].'" target="_blank"><img src="'.base_url().'assets/pdf/pdf.png" width="150px"></a>
-			    	<legend>'.$fetch_data['parents_consent'].'</legend>';
-			    	if($fetch_data['consent_status'] == 0) {
-			    		echo '<div class="btn-group btn-group-toggle" data-toggle="buttons">';
-			    		echo '<a href="'.base_url('admin/confirm_consent/'.$fetch_data['username'].'').'" class="btn btn-success"><i class="fas fa-check"></i></a>';
-			    		echo '<a href="'.base_url('admin/decline_consent/'.$fetch_data['username'].'').'" class="btn btn-danger"><i class="fas fa-times"></i></a>';
-			    		echo '</div>';
-			    	}
-			    	else{
-			    		echo 'Status: Approved';
-			    	}
-			    	
+                <?php
+                if(!empty($fetch_data['parents_consent'])) {
+				  	if($fetch_data['parents_consent'] != 'no_pdf.png') {
+				  		echo '<a href="'.base_url($fetch_data['parents_consent']).'" target="_blank"><img src="'.base_url().'assets/pdf/pdf.png" width="150px"></a>
+				    	<legend>'.$fetch_data['parents_consent'].'</legend>';
+				    	if($fetch_data['consent_status'] == 0) {
+				    		echo '<div class="btn-group btn-group-toggle" data-toggle="buttons">';
+				    		echo '<a href="'.base_url('admin/confirm_consent/'.$fetch_data['username'].'').'" class="btn btn-success"><i class="fas fa-check"></i></a>';
+				    		echo '<a href="'.base_url('admin/decline_consent/'.$fetch_data['username'].'').'" class="btn btn-danger"><i class="fas fa-times"></i></a>';
+				    		echo '</div>';
+				    	}
+				    	else{
+				    		echo 'Status: Approved';
+				    	}
+				    } else {
+				    	echo '<legend>No PDF Uploaded</legend>';
+				    }
 			  	}
 			  	else{
 			  		echo '<legend>No PDF Uploaded</legend>';
