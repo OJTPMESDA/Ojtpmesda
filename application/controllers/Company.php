de8809b7174370b98180f5e89abd40894a2a3ce2
Mark Anthony Naluz <mnaluz@teko.ph>
2020-07-01 08:48:15 +0800

    js

--- application/controllers/Company.php
+++ application/controllers/Company.php
@@ -31,53 +31,50 @@ class Company extends MY_Controller {
 
 	public function submitEvaluate($id)
 	{
-		if ($this->input->is_ajax_request()) {
+		$this->load->model('Students_rating_model');
 
-			$this->load->model('Students_rating_model');
+		slack(json_encode($this->input->post(NULL, true)), 'development', 'evaluate');
 
-			slack(json_encode($this->input->post(NULL, true)), 'development', 'evaluate');
+		$this->form_validation->set_rules('ability', 'Ability to perform the assigned work well', 'xss_clean|trim|required');
+		$this->form_validation->set_rules('accuracy', 'Accuracy of work', 'xss_clean|trim|required');
+		$this->form_validation->set_rules('volume', 'Volume of Work Accomplished', 'xss_clean|trim|required');
+		$this->form_validation->set_rules('knowledge', 'Knowledge of the Basic Principle Neccessary for the Accomplishment of assigned Work', 'xss_clean|trim|required');
+		$this->form_validation->set_rules('extent', 'Extent of Knowledge with regards to Department Operations', 'xss_clean|trim|required');
+		$this->form_validation->set_rules('follow', 'Ability to follow Instructions', 'xss_clean|trim|required');
+		$this->form_validation->set_rules('punctuality', 'Punctuality', 'xss_clean|trim|required');
+		$this->form_validation->set_rules('attendance', 'Attendance', 'xss_clean|trim|required');
+		$this->form_validation->set_rules('department', 'Department/Behavior', 'xss_clean|trim|required');
+		$this->form_validation->set_rules('industry', 'Industry', 'xss_clean|trim|required');
+		$this->form_validation->set_rules('interested', 'Interested and Enthusiasm in the Performance of Work', 'xss_clean|trim|required');
+		$this->form_validation->set_rules('orderliness', 'Orderliness', 'xss_clean|trim|required');
+		$this->form_validation->set_rules('remarks', 'Remarks', 'xss_clean|trim');
 
-			$this->form_validation->set_rules('ability', 'Ability to perform the assigned work well', 'xss_clean|trim|required');
-			$this->form_validation->set_rules('accuracy', 'Accuracy of work', 'xss_clean|trim|required');
-			$this->form_validation->set_rules('volume', 'Volume of Work Accomplished', 'xss_clean|trim|required');
-			$this->form_validation->set_rules('knowledge', 'Knowledge of the Basic Principle Neccessary for the Accomplishment of assigned Work', 'xss_clean|trim|required');
-			$this->form_validation->set_rules('extent', 'Extent of Knowledge with regards to Department Operations', 'xss_clean|trim|required');
-			$this->form_validation->set_rules('follow', 'Ability to follow Instructions', 'xss_clean|trim|required');
-			$this->form_validation->set_rules('punctuality', 'Punctuality', 'xss_clean|trim|required');
-			$this->form_validation->set_rules('attendance', 'Attendance', 'xss_clean|trim|required');
-			$this->form_validation->set_rules('department', 'Department/Behavior', 'xss_clean|trim|required');
-			$this->form_validation->set_rules('industry', 'Industry', 'xss_clean|trim|required');
-			$this->form_validation->set_rules('interested', 'Interested and Enthusiasm in the Performance of Work', 'xss_clean|trim|required');
-			$this->form_validation->set_rules('orderliness', 'Orderliness', 'xss_clean|trim|required');
-			$this->form_validation->set_rules('remarks', 'Remarks', 'xss_clean|trim');
+		if ($this->form_validation->run()) {
+			$save = [
+				'studentID'	=> $id,
+				'rating_1' => $this->input->post('ability'),
+				'rating_2' => $this->input->post('accuracy'),
+				'rating_3' => $this->input->post('volume'),
+				'rating_4' => $this->input->post('knowledge'),
+				'rating_5' => $this->input->post('extent'),
+				'rating_6' => $this->input->post('follow'),
+				'rating_7' => $this->input->post('punctuality'),
+				'rating_8' => $this->input->post('attendance'),
+				'rating_9' => $this->input->post('department'),
+				'rating_10' => $this->input->post('industry'),
+				'rating_11' => $this->input->post('interested'),
+				'rating_12' => $this->input->post('orderliness'),
+				'remarks' => $this->input->post('remarks'),
+				'rating_by' => $this->session->uid
+			];
 
-			if ($this->form_validation->run()) {
-				$save = [
-					'studentID'	=> $id,
-					'rating_1' => $this->input->post('ability'),
-					'rating_2' => $this->input->post('accuracy'),
-					'rating_3' => $this->input->post('volume'),
-					'rating_4' => $this->input->post('knowledge'),
-					'rating_5' => $this->input->post('extent'),
-					'rating_6' => $this->input->post('follow'),
-					'rating_7' => $this->input->post('punctuality'),
-					'rating_8' => $this->input->post('attendance'),
-					'rating_9' => $this->input->post('department'),
-					'rating_10' => $this->input->post('industry'),
-					'rating_11' => $this->input->post('interested'),
-					'rating_12' => $this->input->post('orderliness'),
-					'remarks' => $this->input->post('remarks'),
-					'rating_by' => $this->session->uid
-				];
+			$res = $this->Students_rating_model->_insert($save);
 
-				$res = $this->Students_rating_model->_insert($save);
-
-				if ($res) {
-					$this->response(true, null, ['action' => 'redirect', 'url' => base_url('student/confirm/list')]);
-				}
-
-				$this->response(false);
+			if ($res) {
+				$this->response(true, null, ['action' => 'redirect', 'url' => base_url('student/confirm/list')]);
 			}
+
+			$this->response(false);
 		}
 	}
 }
