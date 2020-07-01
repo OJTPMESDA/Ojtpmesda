04bd9e689dc20cd5cab9b074027d31f107edbbba
Mark Anthony Naluz <mnaluz@teko.ph>
2020-07-01 22:28:54 +0800

    chart container

--- application/views/pages/student_profile.php
+++ application/views/pages/student_profile.php
@@ -85,14 +85,16 @@
 			    <p>Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip jean shorts ullamco ad vinyl cillum PBR. Homo nostrud organic, assumenda labore aesthetic magna delectus mollit.</p>
 			  </div>
 			</div>
+			<?php if (!empty($ratings)): ?>
 			<div class="row">
 				<div class="col-md-3">
 					<div id="chart"></div>
 				</div>
-				<div class="col-md-6">
+				<div class="col-md-9">
 					<div id="bar-chart"></div>
 				</div>
 			</div>
+			<?php endif; ?>
 		</div>	
 			
 	</div>
