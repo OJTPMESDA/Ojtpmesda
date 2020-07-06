7e96c5f04d620cad53fcec5a3e7793f8584489f6
Mark Anthony Naluz <mnaluz@teko.ph>
2020-07-06 11:14:53 +0800

    link

--- application/views/partners/includes/nav-bar.php
+++ application/views/partners/includes/nav-bar.php
@@ -6,6 +6,6 @@
 				<a class="nav-link" href="<?php echo base_url('students/confirm/list'); ?>">Students</a>
 			</li>
 			<li class="nav-item">
-				<a class="nav-link" href="<?php echo base_url('users/list'); ?>">Users</a>
+				<a class="nav-link" href="<?php echo base_url('user/list'); ?>">Users</a>
 			</li>
 		</ul>
